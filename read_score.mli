746e2996752198e21549788e627b7beffa67ebf3
Benlog <loglisci@hotmail.fr>
2016-04-14 21:51:04 +0200

    retrait du treestructure inutile

--- read_score.mli
+++ read_score.mli
@@ -11,12 +11,3 @@ module type READSCORE =
 		val lecture : string -> int list
 	end
 ;;
-
-module type TREESTRUCTURE =
-	sig
-		type holy_tree
-		val init_tree : holy_tree
-		val construire_arbre : int list -> holy_tree
-		val parcours_arbre : holy_tree -> int list
-	end
-;;
